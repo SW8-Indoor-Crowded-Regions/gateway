dac9e7d61127d684cc78504de37d265f607d29dd
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-20 11:24:06 +0100

    feat: lint + unit test workflows

--- app/routes/gateway.py
+++ app/routes/gateway.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 from pydantic import BaseModel
 
 # from app.utils.forwarder import forward_request
---
+++ app/test/local/test_big_shq.py
@@ -0,0 +1,5 @@
+def test_dummy():
+    """A test that always passes, as long as fundmental math is not drastically changed"""
+    big_shaq = 2 + 2
+    assert big_shaq == 4
+    assert big_shaq - 1 == 3
