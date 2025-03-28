7709cb74e0e31c91df34e2ee797dada56c35a83f
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-28 11:26:40 +0100

    feat: added sensor routes + examples
    Also removed test routes and included from new file instead

--- app/routes/api_routes.py
+++ app/routes/api_routes.py
@@ -3,24 +3,14 @@ from pydantic import BaseModel
 from app.schemas.pathfinding_schema import path_finding_request
 from app.services.pathfinding_service import calculate_fastest_path
 from app.routes.rooms.room_routes import router as room_router
+from app.routes.sensors.sensor_routes import router as sensor_router
+from app.routes.tests.test_routes import router as test_router
 
 router = APIRouter()
 
 router.include_router(room_router, prefix="/rooms", tags=["rooms"])
-
-@router.get("/test",
-            summary="Test if the API is working",
-				description="A simple test route to check if the API is working.",
-				tags=["test"],
-				response_description="A simple message to confirm the API is working.",
-				response_model=dict
-    		)
-async def get_route_test():
-    return {"message": "API is working!"}
-
-@router.post("/test")
-async def post_route_test(request: BaseModel):
-    return {"message": "Received test data."}
+router.include_router(sensor_router, prefix="/sensors", tags=["sensors"])
+router.include_router(test_router, prefix="/test", tags=["test"])
 
 @router.post("/fastest-path")
 async def get_fastest_path(request: path_finding_request):
