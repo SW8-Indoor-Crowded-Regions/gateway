0c850b34e9eefe870c810cf60c5a8fdbbe8e0098
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-28 12:18:21 +0100

    fix: formatting

--- app/routes/tests/test_routes.py
+++ app/routes/tests/test_routes.py
@@ -3,16 +3,19 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
-@router.get("/test",
-            summary="Test if the API is working",
-				description="A simple test route to check if the API is working.",
-				tags=["test"],
-				response_description="A simple message to confirm the API is working.",
-				response_model=BaseModel
-    		)
-async def get_route_test():
-    return {"message": "API is working!"}
 
-@router.post("/test")
+@router.get(
+	'/',
+	summary='Test if the API is working',
+	description='A simple test route to check if the API is working.',
+	tags=['test'],
+	response_description='A simple message to confirm the API is working.',
+	response_model=dict[str, str],
+)
+async def get_route_test():
+	return {'message': 'API is working!'}
+
+
+@router.post('/')
 async def post_route_test(request: BaseModel):
-    return {"message": "Received test data."}
+	return {'message': 'Received test data.'}
