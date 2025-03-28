a3e3a9e35f7f3e3b4c6e62050a1b7cf90831999d
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-03-28 10:06:26 +0100

    fix: removed dummy tests

--- app/test/e2e/test_dummy.py
+++
@@ -1,5 +0,0 @@
-def test_dummy():
-    """A test that always passes, as long as fundamental math is not drastically changed"""
-    big_shaq = 2 + 2
-    assert big_shaq == 4
-    assert big_shaq - 1 == 3
