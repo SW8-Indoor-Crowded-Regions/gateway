3be8455023824078e5e79043a1573b33362cc03d
Morten Munk <mortenmunk97@gmail.com>
2025-05-06 10:01:42 +0200

    is_vertical

--- app/schemas/sensor_response_schema.py
+++ app/schemas/sensor_response_schema.py
@@ -1,16 +1,21 @@
 from pydantic import BaseModel
 from typing import List
 
+
 class SensorModel(BaseModel):
 	id: str
 	rooms: List[str]
 	latitude: float
-	longitude: float		  
+	longitude: float
+	is_vertical: bool
+
 
 class SensorListModel(BaseModel):
-  sensors: List[SensorModel]
+	sensors: List[SensorModel]
+
 
 class SensorNoRoomsModel(BaseModel):
 	id: str
 	latitude: float
 	longitude: float
+
